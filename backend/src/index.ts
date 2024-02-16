24b83df9f615c53317c34ba17581475155eb14fc
ritzDev07 <ritzocite@gmail.com>
2024-02-16 15:38:51 +0800

    MongoDb setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,19 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+const PORT = process.env.PORT || 4100;
+
+// Connect to MongoDB using the provided connection string
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
 
 const app = express();
 app.use(express.json());
@@ -11,6 +24,6 @@ app.get("/test", async (req: Request, res: Response) => {
 });
 
 
-app.listen(4000, () => {
-    console.log("server started on localhost:4000")
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
