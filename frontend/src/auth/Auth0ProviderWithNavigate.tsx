11fa6eba1720d1feec2589653be19608e9b2fe37
ritzDev07 <ritzocite@gmail.com>
2024-02-19 15:32:30 +0800

    Added AUTH0 setup

---
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,39 @@
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+
+    // Checking if any configuration value is missing
+    if (!domain || !clientId || !redirectUri) {
+        throw new Error("Unable to initialise auth");
+    }
+
+    // Callback function invoked upon authentication redirect
+    const onRedirectCallback = (appState?: AppState, user?: User) => {
+        // Logging the authenticated user
+        console.log("User:", user);
+    }
+
+    // Rendering Auth0Provider with configured properties
+    return (
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{
+                redirect_uri: redirectUri,
+            }}
+            onRedirectCallback={onRedirectCallback}
+        >
+            {children}
+        </Auth0Provider>
+    )
+};
+
+export default Auth0ProviderWithNavigate;
--- frontend/src/components/mainNav/MainNav.tsx
+++ frontend/src/components/mainNav/MainNav.tsx
@@ -1,11 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "../ui/button";
 
 
 const MainNav = () => {
+    const { loginWithRedirect } = useAuth0();
     return (
         <Button
             variant="ghost"
             className="font-bold border bg-green-600 text-white border-green-600 hover:text-green-600 hover:bg-white"
+            onClick={async () => await loginWithRedirect()}
         >
             Log In
         </Button>
