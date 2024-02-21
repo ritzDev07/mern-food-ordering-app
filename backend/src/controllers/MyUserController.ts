355f0f10a8d28bf78fbe08044b01b6804ef2e4eb
ritzDev07 <ritzocite@gmail.com>
2024-02-21 15:09:21 +0800

    Update user Profile API and Validations

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -17,7 +17,7 @@ const createCurrentUser = async (req: Request, res: Response) => {
 
         // return the user object to the calling client
         res.status(201).json(newUser.toObject());
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -27,6 +27,34 @@ const createCurrentUser = async (req: Request, res: Response) => {
     }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+    try {
+        const { name, addressLine1, country, city } = req.body;
+        const user = await User.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not Found!"
+            });
+        }
+
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.city = city;
+        user.country = country;
+
+        await user.save();
+        res.send(user);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error updating User!"
+        });
+    }
+};
+
 export default {
     createCurrentUser,
+    updateCurrentUser,
 };
